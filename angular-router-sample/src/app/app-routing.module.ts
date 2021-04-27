f9ffd2f7de91fca2a3e30935da47d946e48bfdcf
Ty Foster <tyfoster@outlook.com>
2021-04-26 20:57:20 -0700

    Implements 404 error

--- angular-router-sample/src/app/app-routing.module.ts
+++ angular-router-sample/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [];
 
@@ -10,6 +11,7 @@ const routes: Routes = [];
     { path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
     { path: 'crisis-list', component: CrisisListComponent },
     { path: 'heroes-list', component: HeroesListComponent },
+    { path: '**', component: PageNotFoundComponent }
   ])],
   exports: [RouterModule]
 })
--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
