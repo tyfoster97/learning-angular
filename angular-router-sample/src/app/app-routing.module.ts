a7531a16fcdc3fbba8a418023d2e7fd165e452d4
Ty Foster <tyfoster@outlook.com>
2021-04-26 20:42:58 -0700

    Sets up redirect

--- angular-router-sample/src/app/app-routing.module.ts
+++ angular-router-sample/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'crisis-list', component: CrisisListComponent },
+    { path: 'heroes-list', component: HeroesListComponent },
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule,
